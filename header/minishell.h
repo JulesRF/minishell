46443b260499d1359a5b8d81e7a01ef3a87cfb0f
JulesRF <51792641+JulesRF@users.noreply.github.com>
2022-05-05 18:48:38 +0200

    Add files via upload

--- header/minishell.h
+++ header/minishell.h
@@ -13,9 +13,16 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 # include <stdio.h>
+# include <readline/readline.h>
+# include <readline/history.h>
 # include <unistd.h>
 # include <stdlib.h>
 
+typedef struct s_list
+{
+	void    		*content;
+	struct s_list	*next;
+}					t_list;
 //	main.c
 int	main(int argc, char **argv, char **env);
 
--- srcs/main.c
+++ srcs/main.c
@@ -11,8 +11,75 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
-# include <readline/readline.h>
-# include <readline/history.h>
+
+void	ft_garbage(t_list **bin)
+{
+	t_list	*to_suppr;
+
+	while (*bin)
+	{
+		to_suppr = *bin;
+		*bin = to_suppr->next;
+		free(to_suppr->content);
+		free(to_suppr);
+	}
+	*bin = NULL;
+}
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*newcell;
+
+	newcell = malloc(sizeof(t_list));
+	if (!newcell)
+		return (NULL);
+	newcell->content = content;
+	newcell->next = NULL;
+	return (newcell);
+}
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	t_list	*temp;
+
+	if (!*alst)
+	{
+		*alst = new;
+		return ;
+	}
+	temp = ft_lstlast(*alst);
+	temp->next = new;
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (s1[i])
+		i++;
+	dest = malloc(sizeof(char) * i + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -42,17 +109,25 @@ void	ft_preparse(int argc, char **argv, char **env)
 
 int	main(int argc, char **argv, char **env)
 {
-	char	*str;
+	// char	*str;
+	t_list	*bin;
 
 	ft_preparse(argc, argv, env);
-	str = readline("\033[95mminishell$\033[33m ");
-	while (ft_strcmp(str, "exit") && str != NULL)
-	{
-		printf("%s\n", str);
-		add_history(str);
-		free (str);
-		str = readline("\033[95mminishell$\033[33m ");
-	}
-	free (str);
+	bin = NULL;
+
+	ft_lstadd_back(&bin, ft_lstnew(ft_strdup("salut")));
+	ft_lstadd_back(&bin, ft_lstnew(ft_strdup("ca")));
+	ft_lstadd_back(&bin, ft_lstnew(ft_strdup("va ?")));
+	// str = readline("\033[95mminishell$\033[33m ");
+	// while (ft_strcmp(str, "exit") && str != NULL)
+	// {
+	// 	// printf("la string ->%s<-\n", str);
+	// 	if (str[0] != '\0')
+	// 		add_history(str);
+	// 	free (str);
+	// 	str = readline("\033[95mminishell$\033[33m ");
+	// }
+	// free (str);
+	ft_garbage(&bin);
 	exit(0);
 }
