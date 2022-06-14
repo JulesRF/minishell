5c98165c2d12f43b6038ef1f05477e7c8e393758
Vincent Fiszbin <fiszbin.vincent@gmail.com>
2022-06-14 21:20:36 +0200

    Norme search_cmd

--- srcs/search_cmd.c
+++ srcs/search_cmd.c
@@ -6,7 +6,7 @@
 /*   By: vfiszbin <vfiszbin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 16:20:08 by vfiszbin          #+#    #+#             */
-/*   Updated: 2022/06/14 17:14:29 by vfiszbin         ###   ########.fr       */
+/*   Updated: 2022/06/14 21:17:50 by vfiszbin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,52 @@ int	cmd_not_found(t_token *command)
  * @param command Linked list of command (name) and arguments
  * @return int -1 if the search fails, 1 if execution fails, 0 if no error
  */
-int check_builtin(t_token *command, char ***env, t_list **bin, char *cmd_line)
+int	check_builtin(t_token *command, char ***env, t_list **bin, char *cmd_line)
 {
-	char *cmd_name;
-	
-	cmd_name = command->content; 
+	char	*cmd_name;
+
+	cmd_name = command->content;
 	if (ft_strcmp(cmd_name, "echo") == 0)
-		return echo(command);
+		return (echo(command));
 	if (ft_strcmp(cmd_name, "cd") == 0)
-		return cd(command, env);
+		return (cd(command, env));
 	if (ft_strcmp(cmd_name, "pwd") == 0)
-		return pwd();
+		return (pwd());
 	if (ft_strcmp(cmd_name, "export") == 0)
 		return (export(command, env));
 	if (ft_strcmp(cmd_name, "unset") == 0)
-		return unset(command, env);
+		return (unset(command, env));
 	if (ft_strcmp(cmd_name, "env") == 0)
 		return (env_builtin(*env));
 	if (ft_strcmp(cmd_name, "exit") == 0)
 		return (exit_builtin(command, env, bin, cmd_line));
-	return -1;
+	return (-1);
+}
+
+int	find_executable(t_vars *vars, char **paths, char *cmd_name)
+{
+	char		*path_to_cmd;
+	int			i;
+	struct stat	sb;
+	t_list		*node;
+
+	i = 0;
+	while (paths[i])
+	{
+		path_to_cmd = ft_strjoin(paths[i], cmd_name);
+		if ((stat(path_to_cmd, &sb) == 0 && sb.st_mode & S_IXUSR))
+		{
+			node = ft_lstnew(path_to_cmd);
+			if (!node)
+				return (-1);
+			ft_lstadd_back(vars->bin, node);
+			(*(vars->cmd))->content = path_to_cmd;
+			return (exec_cmd((*(vars->cmd)), *(vars->env), vars->pid));
+		}
+		i++;
+		free(path_to_cmd);
+	}
+	return (-1);
 }
 
 /**
@@ -57,19 +83,17 @@ int check_builtin(t_token *command, char ***env, t_list **bin, char *cmd_line)
  * @param env Environment variables
  * @return int -1 if the search or execution fails
  */
-int check_path(t_token *command, char **env, t_list **bin, pid_t pid)
+int	check_path(t_vars *vars)
 {
-	char *cmd_name;
-	char *path;
-	char *path_to_cmd;
-	char **paths;
-	struct stat sb;
-	int i;
+	char		*cmd_name;
+	char		*path;
+	char		**paths;
+	int			ret;
 
-	cmd_name = ft_strjoin("/", command->content);
+	cmd_name = ft_strjoin("/", (*(vars->cmd))->content);
 	if (!cmd_name)
 		return (-1);
-	path = get_env_value("PATH", env);
+	path = get_env_value("PATH", *(vars->env));
 	if (!path)
 	{
 		free(cmd_name);
@@ -82,26 +106,11 @@ int check_path(t_token *command, char **env, t_list **bin, pid_t pid)
 		free(cmd_name);
 		return (-1);
 	}
-	i = 0;
-	while (paths[i])
-	{
-		path_to_cmd = ft_strjoin(paths[i], cmd_name);
-		if ((stat(path_to_cmd, &sb) == 0 && sb.st_mode & S_IXUSR)) //check if executable
-		{
-			ft_lstadd_back(bin, ft_lstnew(path_to_cmd));//PROTECT
-			command->content = path_to_cmd;
-			free(cmd_name);
-			free_strs(paths);
-			return exec_cmd(command, env, pid);
-		}
-		i++;
-		free(path_to_cmd);
-	}
+	ret = find_executable(vars, paths, cmd_name);
 	free(cmd_name);
 	free_strs(paths);
-	return (-1);
+	return (ret);
 }
-
 
 /**
  * @brief Search the command executable and try execute it
@@ -124,7 +133,7 @@ int	search_cmd(t_vars *vars)
 		ret = check_builtin(*(vars->cmd), vars->env, vars->bin, vars->cmd_line);
 		if (ret != -1)
 			return (ret);
-		ret = check_path(*(vars->cmd), *(vars->env), vars->bin, vars->pid);
+		ret = check_path(vars);
 		if (ret != -1)
 			return (ret);
 		return (cmd_not_found(*(vars->cmd)));
